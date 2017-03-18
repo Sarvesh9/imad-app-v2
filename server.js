3806bef897731ebe145da60831c3d4fb0ef1d36a
Sarvesh9 <sarveshnarkar2d104@gmail.com>
2017-03-18 13:45:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,14 +105,14 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
   
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){ 
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result) { 
       if (err) {
           res.status(500).send(err.toString());
       } else {
           if (result.length.rows === 0) {
               res.status(404).send('Article not found');
           } else {
-              var articleData = result.row[0];
+              var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
       }
